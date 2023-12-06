0f0419560a6f1c16a1cfb5c0ddb45f6b04a0ca68
Aaron Deadman <aaron@introspection.net.au>
2023-12-06 13:45:05 +0000

    AOC2023 day 4 part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn part1(input: &str) {
     println!("AOC Day 4 Part 1");
@@ -19,6 +19,26 @@ fn part1(input: &str) {
 
 fn part2(input: &str) {
     println!("AOC Day 4 Part 2");
+    let mut scratchcards = HashMap::<u32, u32>::new();
+    for line in input.lines() {
+        let (card, numbers) = line.split_once(": ").unwrap();
+        let current_card_number = card.split_whitespace().collect::<Vec<_>>()[1].parse::<u32>().unwrap();
+        scratchcards.entry(current_card_number).and_modify(|e| {*e += 1}).or_insert(1);
+        let number_current_scratchcards = scratchcards[&current_card_number];
+        let (winning_numbers, card_numbers) = numbers.split_once(" | ").unwrap();
+        let winning_numbers: HashSet<&str> = HashSet::from_iter(winning_numbers.split_whitespace().collect::<Vec<&str>>().iter().cloned());
+        let card_numbers: HashSet<&str> = HashSet::from_iter(card_numbers.split_whitespace().collect::<Vec<&str>>().iter().cloned());
+        let matches = <std::collections::hash_set::Intersection<'_, &str, _> as Iterator>::count(card_numbers.intersection(&winning_numbers)) as u32;
+        if matches > 0 {
+            for card_number in (current_card_number+1)..=(current_card_number+matches) {
+                scratchcards.entry(card_number).and_modify(|e| {
+                    *e += number_current_scratchcards
+                }).or_insert(number_current_scratchcards);
+            }
+        }
+    }
+    let total_scratchcards: u32 = scratchcards.values().cloned().collect::<Vec<u32>>().iter().sum();
+    println!("Total: {}", total_scratchcards);
 }
 
 fn main() {
